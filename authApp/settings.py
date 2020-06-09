e0a4ad2397dea66ff620474e7efa33df34c83633
Andrii <andremailboxua@gmail.com>
2020-06-09 12:52:41 +0300

    09-06-2020 -- 12:54

--- authApp/settings.py
+++ authApp/settings.py
@@ -173,6 +173,7 @@ SOCIALACCOUNT_PROVIDERS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
