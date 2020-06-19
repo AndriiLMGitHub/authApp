f166ffab6d0551288c5b9430f68592c0435d2b75
Andrii <andremailboxua@gmail.com>
2020-06-19 17:41:53 +0300

    19-06-2020 -- 1.6

--- commodities/models.py
+++ commodities/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-import PIL
 
 # Create your models here.
 
@@ -9,14 +8,14 @@ class CommodityItem(models.Model):
     price_good = models.CharField(max_length = 10)
     description_good = models.TextField(max_length = 1000)
     date_published_good = models.DateTimeField(auto_now_add=True)
-    image_good = models.ImageField(upload_to='images_goods/',)
-    image_good_galery_1 = models.ImageField(upload_to='images_goods',)
-    image_good_galery_2 = models.ImageField(upload_to='images_goods/',)
-    image_good_galery_3 = models.ImageField(upload_to='images_goods/',)
-    image_good_galery_4 = models.ImageField(upload_to='images_goods/',)
-    image_good_galery_5 = models.ImageField(upload_to='images_goods/',)
-    image_good_galery_6 = models.ImageField(upload_to='images_goods/',)
-    image_good_galery_7 = models.ImageField(upload_to='images_goods/gallery',)
+    image_good = models.FileField(upload_to='images_goods/',)
+    image_good_galery_1 = models.FileField(upload_to='images_goods',)
+    image_good_galery_2 = models.FileField(upload_to='images_goods/',)
+    image_good_galery_3 = models.FileField(upload_to='images_goods/',)
+    image_good_galery_4 = models.FileField(upload_to='images_goods/',)
+    image_good_galery_5 = models.FileField(upload_to='images_goods/',)
+    image_good_galery_6 = models.FileField(upload_to='images_goods/',)
+    image_good_galery_7 = models.FileField(upload_to='images_goods/gallery',)
 
     def __str__(self):
         return self.name_good
