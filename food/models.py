db67f958c82c6490b1eb6812ee816dce8d5ac8ab
Andrii <andremailboxua@gmail.com>
2020-07-08 18:53:44 +0300

    food models

--- food/models.py
+++ food/models.py
@@ -5,7 +5,7 @@ class FoodItem(models.Model):
     titleFood = models.CharField(max_length=30)
     textFood = models.TextField()
     date_published = models.DateTimeField()
-    imageFood = models.ImageField(upload_to='FoodImages')
+    imageFood = models.FileField(upload_to='FoodImages')
 
 
     def __str_(self):
